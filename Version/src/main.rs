13d74a42d55474663c85d1e54e5247e56996e4ea
Andrew Parker <andrew.parker@beslogic.com>
2022-09-09 20:33:33 -0400

    making an api out of env

--- Version/src/main.rs
+++ Version/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> String {
+    return env::var("VERSION").unwrap_or("none".to_string());
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
