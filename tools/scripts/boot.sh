0c2f12583121454d53cb5f98bc4b189c48070f72
Andrew Parker <andrew.parker@beslogic.com>
2022-09-16 15:27:15 -0400

    adding logging for the echo

--- tools/scripts/boot.sh
+++ tools/scripts/boot.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "1" > 1.txt
 cd /home/ubuntu/Besbot/tools/scripts/
+echo "2" > 1.txt
 git pull
+echo "3" > 1.txt
 docker image prune -a -f
+echo "4" > 1.txt
 docker-compose pull && docker-compose up -d
+echo "5" > 1.txt
 
 echo "test it should be present #2"
