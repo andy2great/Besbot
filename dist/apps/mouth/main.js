317b4758f107b0548048b095ea0ea239b7f3f6da
Andrew Parker <andrew.parker@beslogic.com>
2022-09-27 00:23:52 -0400

    updating to not crash

--- dist/apps/mouth/main.js
+++ dist/apps/mouth/main.js
@@ -55,14 +55,13 @@ const app = express();
 app.use(express.json());
 app.post('/', (req, res) => {
     say.speak(req.body.text);
+    console.log(`received ${req.body.text}`);
     res.send(null);
 });
 const port = process.env.port || 8081;
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}/`);
 });
-say.getInstalledVoices(console.log);
-console.log('ok');
 
 })();
 
